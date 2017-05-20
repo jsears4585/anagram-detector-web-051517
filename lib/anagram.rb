e74418d05ae71a885a0970c032021a8c6f20db32
Jason Sears <jsears4585@gmail.com>
2017-05-20 14:46:19 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,10 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :input
+  def initialize input
+    @input = input
+    @chars = input.split("").sort
+  end
+  def match match_ary
+    match_ary.map {|w| w if @chars.eql?(w.split("").sort)}.compact
+  end
+end
